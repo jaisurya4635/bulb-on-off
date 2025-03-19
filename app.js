d3996e21858f2e28f6a73b1a6cea4ffd5d7b4f0e
jaisurya4635 <jaisuryajaisurya623@gmail.com>
2025-03-19 20:51:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src ="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg"
+}
+
+function bulb_off()
+{
+    img.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhrksrUA2a0NnbCk_jbt-M3O_fwGMDY_Yuw&s"
+}
